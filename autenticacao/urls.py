9471952a3f6db8f0d4d4c13caa15c58d0ceea155
itallume <itallo.oliver21@gmail.com>
2025-05-06 10:52:13 -0300

    feat: botão de logout

--- autenticacao/urls.py
+++ autenticacao/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='login/login.html'), name='login'),
-    #path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('registro/', views.registrar_usuario, name='registro'),
     path('logado/', views.logado, name='logado'),
 ]
