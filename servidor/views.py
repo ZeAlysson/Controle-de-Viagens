38134c240989c193246ea5018432931786c8347e
itallume <itallo.oliver21@gmail.com>
2025-05-14 11:39:22 -0300

    feat: ambiente crud servidor para admin

--- servidor/views.py
+++ servidor/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import redirect, render
-
-from autenticacao.forms import LoginServidoresForm
+from django.shortcuts import get_object_or_404, redirect, render
 from controle.models import Controle
+from servidor.forms import ServidorForm
 from servidor.models import Servidor
 from django.contrib import messages
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def listar_viagens(request):
@@ -24,3 +25,39 @@ def listar_viagens(request):
     
     viagens = Controle.objects.filter(servidor=servidor)
     return render(request, 'global/templates/listagem_viagens.html', {'viagens': viagens})
+
+@login_required
+def cadastrar_servidor(request):
+    if request.method == 'POST':
+        form = ServidorForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_servidores')
+    else:
+        form = ServidorForm()
+
+    return render(request, 'servidor/cadastrar_servidor.html', {'form': form})
+
+@login_required
+def listar_servidores(request):
+    servidores = Servidor.objects.all()
+    return render(request, 'servidor/listar_servidores.html', {'servidores': servidores})
+
+@login_required
+def excluir_servidor(request, servidor_cpf):
+    servidor = get_object_or_404(Servidor, pk=servidor_cpf)
+    servidor.delete()
+    return redirect('listar_servidores')
+
+@login_required
+def editar_servidor(request, servidor_cpf):
+    servidor = get_object_or_404(Servidor, pk=servidor_cpf)
+
+    if request.method == 'POST':
+        form = ServidorForm(request.POST, instance=servidor)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_servidores')
+    else:
+        form = ServidorForm(instance=servidor)
+    return render(request, 'servidor/editar_servidor.html', {'form': form, 'servidor': servidor})
